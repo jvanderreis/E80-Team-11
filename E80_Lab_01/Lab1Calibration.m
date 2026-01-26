7996344c70b70db500c231868edf7c8f783f7fc2
jvanderreis <jvanderreis@g.hmc.edu>
2026-01-25 19:22:30 -0800

    a

--- E80_Lab_01/Lab1Calibration.m
+++ E80_Lab_01/Lab1Calibration.m
@@ -3,6 +3,8 @@
 % unknown)
 clear; clc; close all;
 
+
+
 % Load Flat File
 filenum = '001'; 
 logreader; 
@@ -20,14 +22,6 @@ Scale_Factor = 9.81 / (mean_1g - mean_0g);
 
 fprintf('The Teensy Scale Factor is %.6f', Scale_Factor);
 
-% T-Test (Zero X vs Zero Y), determines if zero results are statistically
-% similar and therefore accurate (H0 = X and Y are similar, Ha = X and Y
-% are dissimilar and something is going wrong). "h" determines if the null
-% should be rejected (1 = reject, 0 = accept)
-
-[h, p] = ttest2(ZeroX, ZeroY);
-fprintf('T-Test P-Value for X and Y is %.4e so h is %d (If h is 1, reject; If h is 0 accept)\n', p, h);
-
 % Z axis Testing
 
 TrueZ = GravityZ * Scale_Factor;
@@ -43,4 +37,24 @@ fprintf('The mean value for Z acceleration is %.4f m/s^2\n', Zmean);
 fprintf('The standard deviation for Z is %.4f\n', Zstd);
 fprintf('The Standard Error for Z is %.4f\n', Zse);
 fprintf('The 95%% CI Bounds are [%.4f, %.4f]\n', ConfidenceLower, ConfidenceUpper);
+
+% T-Tests determine if "zero" results are statistically
+% similar and therefore accurate (e.g. H0 = X and Y are similar, Ha = X and Y
+% are dissimilar and something is going wrong). "h" determines if the null
+% should be rejected (1 = reject, 0 = accept)
+
+% T-Test 1: Zero X vs Zero Y
+[h, p] = ttest2(ZeroX, ZeroY);
+fprintf('X vs Y: P-Value is %.4e (h=%d) -> ', p, h);
+if h==1, fprintf('Different\n'); else, fprintf('Similar\n'); end
+
+% T-Test 2: Zero X vs Zero Z (Using Side Data for Z)
+[h2, p2] = ttest2(ZeroX, ZeroZ);
+fprintf('X vs Z: P-Value is %.4e (h=%d) -> ', p2, h2);
+if h2==1, fprintf('Different\n'); else, fprintf('Similar\n'); end
+
+% T-Test 3: Zero Y vs Zero Z (Using Side Data for Z)
+[h3, p3] = ttest2(ZeroY, ZeroZ);
+fprintf('Y vs Z: P-Value is %.4e (h=%d) -> ', p3, h3);
+if h3==1, fprintf('Different\n'); else, fprintf('Similar\n'); end
 

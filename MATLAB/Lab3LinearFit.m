84c623d89a4e9de16f9dbf71b094f4862b36bfc1
soniaHMC <sberliner@hmc.edu>
2026-02-08 19:11:32 -0800

    hopefully correct

--- MATLAB/Lab3LinearFit.m
+++ MATLAB/Lab3LinearFit.m
@@ -1,24 +1,27 @@
 %Linear fitter for lab 3
 %Written by Sonia Berliner
-scopeData = readtable('"filename".csv', 'PreserveVariableNames', true) % filename is a placeholder, fill it in with the name of the file being analyzed
-sampleNumber = scopeData(:,1); % x-axis for the linear plot, name may change depending on what we are measuring
-output = scopeData(:,2); %y-axis for linear plot
+Data = readtable('"filename".csv', 'PreserveVariableNames', true) % filename is a placeholder, fill it in with the name of the file being analyzed.
+% Download the spreadsheet with data in it as a .csv file with a unique and
+% descriptive name.
+turbidity = Data(:,1); % x-axis for the linear plot, name may change depending on what happens in office hours
+voltage = Data(:,2); %y-axis for linear plot, measures voltage ratios
+
 
 format long %for more precision
 
-padded = [ones(length(scopeData),1) scopeData]; %this adds a column of ones to facilitate calculating the intercept
+padded = [ones(length(turbidity),1) turbidity]; %this adds a column of ones to facilitate calculating the intercept
 
-regression = padded\output %regression with y-intercept value
+regression = padded\voltage; %regression with y-intercept value
 
 yvalues = padded*regression; %this is the vector of y-values of the regression
-plot(sampleNumber,yvalues,'--')
+plot(turbidity,yvalues,'--')
 legend('Data','Slope & Intercept');
 
 
-scatter(sampleNumber,output)
+scatter(turbidity,voltage)
 hold on
 plot(sampleNumber,yvalues)
-xlabel('Oscilloscope sample number')
+xlabel('Turbidity')
 ylabel('90 Degree/Transmission Vpp measurement')
-title('Linear Regression of Sample Number vs 90 degree/transmission Vpp')
+title('Linear Regression of Turbidity vs 90 degree/transmission Vpp')
 grid on

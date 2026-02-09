5d15e9aeb7abbbd9ec96bd241b2c49c5c729d876
soniaHMC <sberliner@hmc.edu>
2026-02-09 10:25:13 -0800

    bleh

--- MATLAB/Lab3LinearFit.m
+++ MATLAB/Lab3LinearFit.m
@@ -1,6 +1,6 @@
 %Linear fitter for lab 3
 %Written by Sonia Berliner
-Data = readtable('"filename".csv', 'PreserveVariableNames', true) % filename is a placeholder, fill it in with the name of the file being analyzed.
+Data = readtable('"filename".csv', 'PreserveVariableNames', true); % filename is a placeholder, fill it in with the name of the file being analyzed.
 % Download the spreadsheet with data in it as a .csv file with a unique and
 % descriptive name.
 turbidity = Data(:,1); % x-axis for the linear plot, name may change depending on what happens in office hours
@@ -9,18 +9,19 @@ voltage = Data(:,2); %y-axis for linear plot, measures voltage ratios
 
 format long %for more precision
 l = length(turbidity);
-padded = [[ones(l),1)]' [turbidity]']; %this adds a column of ones to facilitate calculating the intercept
+padded = [ones(l,1) [turbidity]']; %this adds a column of ones to facilitate calculating the intercept
 
-regression = padded\voltage; %regression with y-intercept value
+regression = padded\voltage'; %regression with y-intercept value
 
-yvalues = padded*regression; %this is the vector of y-values of the regression
+yvalues = padded.*regression'; %this is the vector of y-values of the regression
 plot(turbidity,yvalues,'--')
 legend('Data','Slope & Intercept');
 
 
+%this code just plots the data points and linear regression
 scatter(turbidity,voltage)
 hold on
-plot(sampleNumber,yvalues)
+plot(turbidity,yvalues)
 xlabel('Turbidity')
 ylabel('90 Degree/Transmission Vpp measurement')
 title('Linear Regression of Turbidity vs 90 degree/transmission Vpp')
